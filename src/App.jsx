fa2c0646e56d52dbc15c3da41f00b0afc034b127
Kimm-y <kimmiemumbi789@gmail.com>
2024-02-25 19:27:52 +0300

    to do list

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+// import TodoForm from './components/TodoForm'
+import TodoList from './components/TodoList'
+
+function App() {
+
+
+  return (
+    <>
+     <div className='Todo'>
+     <h1>TO DO APP</h1>
+     {/* <TodoForm/> */}
+     <TodoList/>
+
+    
+     </div>
+    </>
+  )
+}
+
+export default App
